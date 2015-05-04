557f15a2684fa43bcf80af0ec1356b8e734117c0
buhr3940 <buhr3940@stthomas.edu>
2015-05-04 16:48:53 -0500

    Form & search query work/correct results in XML
    Search produces XML data file returning correct search results based on
    radius but Google map & markers not rendering

--- phpsqlajax_genxml3.php
+++ phpsqlajax_genxml3.php
@@ -1,10 +1,12 @@
 <?php  
 
 // Get parameters from URL
-//$center_lat = ( isset( $_GET["lat"] ) ? $_GET["lat"] : 0 ); # You could replace these "0"s with the
-//$center_lng = ( isset( $_GET["lng"] ) ? $_GET["lng"] : 0 ); # Lat/Lng of a default location.
-//$radius     = ( isset( $_GET["radius"] ) ? $_GET["radius"] : 10 ); # Again, default radius.
-
+$center_lat = ( isset( $_SESSION['lat'] ) ? $_SESSION['lat'] : 47.6145); # You could replace these "0"s with the
+$center_lng = ( isset( $_SESSION['lng'] ) ? $_SESSION['lng'] : -122.3418 ); # Lat/Lng of a default location.
+$radius     = ( isset( $_POST['radiusSelect'] ) ? $_POST['radiusSelect'] : 10); # Again, default radius.
+//var_dump ($_POST);
+//$radius = htmlspecialchars($_POST['radiusSelect']);
+//echo $radius;
 // Start XML file, create parent node
 $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
@@ -14,34 +16,33 @@ $parnode = $dom->appendChild($node);
 mysql_connect("localhost", "llhosts_ericbuhr", "v9SPf1faFqgn") or die("Could not connect to db");
 mysql_select_db("llhosts_ericbuhr") or die("Could not connect to db");
 //error reporting(0);
-$resource = mysql_query("SELECT * FROM markers limit 10");
+//$resource = mysql_query("SELECT * FROM markers");
 
 // Search the rows in the markers table
-//$query = sprintf( "SELECT address, name, lat, lng, ( 3959 * acos( cos( radians('%s') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( lat ) ) ) ) AS distance FROM markers HAVING distance < '%s' ORDER BY distance LIMIT 0 , 20",
- // mysql_real_escape_string( $center_lat ),
- // mysql_real_escape_string( $center_lng ),
- // mysql_real_escape_string( $center_lat ),
- //mysql_real_escape_string( $radius ) );
- 
-if(!$resource )
-  die( "MySQL Error - Invalid query: " . mysql_error() . ' "'.$query.'"' ); # During debugging, echo the SQL Statement on Error
+$query = sprintf("SELECT address, name, lat, lng, ( 3959 * acos( cos( radians('%s') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( lat ) ) ) ) AS distance FROM markers HAVING distance <= '%s' ORDER BY distance LIMIT 10",
+  mysql_real_escape_string($center_lat),
+  mysql_real_escape_string($center_lng),
+  mysql_real_escape_string($center_lat),
+  mysql_real_escape_string($radius));
 
-if( !headers_sent() )
-  header( "Content-type: text/xml" );
+
+$result = mysql_query($query);
+if (!$result) {
+  die("Invalid query: " . mysql_error());
+}
+
+header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
-if( mysql_num_rows( $resource ) ){
-  while ($row = @mysql_fetch_assoc($resource)){
-    $node = $dom->createElement("marker");
-    $newnode = $parnode->appendChild($node);
-    $newnode->setAttribute("name", $row['name']);
-    $newnode->setAttribute("address", $row['address']);
-    $newnode->setAttribute("lat", $row['lat']);
-    $newnode->setAttribute("lng", $row['lng']);
-    $newnode->setAttribute("type", $row['type']);
-  }
-}else
-  die( 'MySQL Error - No Records Returned "'.$query.'"' );
+while ($row = @mysql_fetch_assoc($result)){
+  $node = $dom->createElement("marker");
+  $newnode = $parnode->appendChild($node);
+  $newnode->setAttribute("name", $row['name']);
+  $newnode->setAttribute("address", $row['address']);
+  $newnode->setAttribute("lat", $row['lat']);
+  $newnode->setAttribute("lng", $row['lng']);
+  $newnode->setAttribute("distance", $row['distance']);
+}
 
 echo $dom->saveXML();
 ?>
